9821dff7353c83aa30c48115eeb88ac2a793940b
Ryan <ryan08100715@gmail.com>
2025-02-06 00:18:47 +0800

    Refactoring powershell7 script

--- apps/shell/powershell7/uninstall.ps1
+++ apps/shell/powershell7/uninstall.ps1
@@ -6,8 +6,8 @@
 $PackageName = "PowerShell"
 $WingetPackageID = "Microsoft.PowerShell"
 
-Write-MyInfo "開始執行 $PackageName 安裝腳本..."
+Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
 
-Install-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
 
 Write-MyInfo ""
