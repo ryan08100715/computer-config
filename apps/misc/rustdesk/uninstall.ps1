74ec4f5daf78866dbc2d9c3abd4c2dadea591731
Ryan <ryan08100715@gmail.com>
2025-02-05 23:08:42 +0800

    Add rustdesk script

---
+++ apps/misc/rustdesk/uninstall.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\winget.ps1
+
+$PackageName = "RustDesk"
+$WingetPackageID = "RustDesk.RustDesk"
+
+Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
+
+Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+
+Write-MyInfo ""
