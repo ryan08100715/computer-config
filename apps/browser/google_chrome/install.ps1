77d722bca973169452b7f700572b4cbb6913c10e
Ryan <ryan08100715@gmail.com>
2025-02-05 14:41:23 +0800

    Refactoring google chrome script

--- apps/browser/google_chrome/install.ps1
+++ apps/browser/google_chrome/install.ps1
@@ -1,10 +1,47 @@
 #!/usr/bin/env pwsh
 
-. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\output.ps1
 
 $PackageName = "Google Chrome"
 $WingetPackageID = "Google.Chrome"
+$Package = Get-WinGetPackage -Id $WingetPackageID
 
-# * 安裝 Google Chrome
-Write-Host "開始執行 $PackageName 安裝腳本" -ForegroundColor Green
-Install-PackageByWinget $WingetPackageID
+Write-MyInfo "開始執行 $PackageName 安裝腳本..."
+
+# 檢查是否已經安裝
+if ($null -eq $Package) {
+  Write-MyInfo "$PackageName 尚未安裝，開始進行安裝"
+  
+  $InstallInfo = Install-WinGetPackage -Id $WingetPackageID
+
+  if ("Ok" -eq $InstallInfo.Status) {
+    Write-MySuccess -Icon "安裝完成"
+  }
+  else {
+    Write-MyError -Icon "安裝失敗"
+    $InstallInfo
+  }
+}
+else {
+  Write-MySuccess -Icon "$PackageName 已安裝"
+
+  Write-MyInfo "檢查是否需要更新"
+  if ($Package.IsUpdateAvailable) {
+    Write-MyWarning -Icon "有新版本可供安裝，開始進行更新"
+
+    $UpdateInfo = Update-WinGetPackage -Id $WingetPackageID
+
+    if ("Ok" -eq $UpdateInfo.Status) {
+      Write-MySuccess -Icon "更新完成"
+    }
+    else {
+      Write-MyError -Icon "更新失敗"
+      $UpdateInfo
+    }
+  }
+  else {
+    Write-MySuccess -Icon "已經是最新版本"
+  }
+}
+
+Write-MyInfo ""
