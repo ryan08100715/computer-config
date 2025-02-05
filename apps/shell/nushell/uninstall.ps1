1351189a77b5068512a493845d2e4d0124d7de13
Ryan <ryan08100715@gmail.com>
2025-02-05 23:20:45 +0800

    Add nushell script

---
+++ apps/shell/nushell/uninstall.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env pwsh
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\winget.ps1
+
+$PackageName = "Nushell"
+$WingetPackageID = "Nushell.Nushell"
+
+Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
+
+Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+
+Write-MyInfo ""
