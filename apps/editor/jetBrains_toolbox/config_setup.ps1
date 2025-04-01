f6027f6a06412f8a24c7d058ac90ba409fab962b
Ryan WSY <git@ryanwsy.simplelogin.com>
2025-04-01 18:53:25 +0800

    Add phpstorm config setup

---
+++ apps/editor/jetBrains_toolbox/config_setup.ps1
@@ -0,0 +1,28 @@
+#!/usr/bin/env pwsh
+
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$FolderName
+)
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\windows_schedule.ps1
+
+Write-MyInfo "開始進行 PHPStorm 配置..."
+
+# * 確保以管理員權限執行
+EnsureAdminRun
+
+$jetbrainsDir = "$env:APPDATA\JetBrains"
+$ideDir = Join-Path $jetbrainsDir $FolderName
+
+# * 配置 Keymaps
+$keymapsDir = Join-Path $ideDir "keymaps"
+if (-not (Test-Path $keymapsDir)) {
+  New-Item -ItemType Directory -Path $keymapsDir | Out-Null
+}
+New-Item -ItemType SymbolicLink -Force -Path "$keymapsDir\VSCode copy.xml" -Target "$PSScriptRoot\config\VSCode copy.xml" | Out-Null
+
+Write-MySuccess -Icon "PHPStorm 配置完成"
+Write-MyInfo ""
