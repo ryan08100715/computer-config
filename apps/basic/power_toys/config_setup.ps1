cbd596e5d591d1ab059ffc204cbbbf1094317a46
Ryan WSY <git@ryanwsy.simplelogin.com>
2025-04-12 14:21:36 +0800

    Update powertoys config_setup

--- apps/basic/power_toys/config_setup.ps1
+++ apps/basic/power_toys/config_setup.ps1
@@ -2,9 +2,7 @@
 
 . $PSScriptRoot\..\..\..\utils\output.ps1
 
-$backupDir = "$HOME\Documents\PowerToys\Backup"
-$fileName = "settings_133872081475133648.ptb"
-Copy-Item -Force -Path "$PSScriptRoot\config\$fileName" -Destination "$backupDir\$fileName"
-Write-MySuccess -Icon "放置備份設定檔到: $backupDir\$fileName"
+& "$PSScriptRoot\tools\place_config_to_powertoys_dir.ps1"
+Write-MySuccess -Icon "放置備份設定檔成功"
 
 Write-MyInfo ""
