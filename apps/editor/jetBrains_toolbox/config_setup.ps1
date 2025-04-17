f08c7916d0db93c5d4eb01492bbfc033b905f168
Ryan WSY <git@ryanwsy.simplelogin.com>
2025-04-17 20:20:06 +0800

    Update phpstorm config

--- apps/editor/jetBrains_toolbox/config_setup.ps1
+++ apps/editor/jetBrains_toolbox/config_setup.ps1
@@ -24,5 +24,13 @@ if (-not (Test-Path $keymapsDir)) {
 }
 New-Item -ItemType SymbolicLink -Force -Path "$keymapsDir\VSCode copy.xml" -Target "$PSScriptRoot\config\VSCode copy.xml" | Out-Null
 
+# * 配置 Options
+$optionsDir = Join-Path $ideDir "options"
+if (-not (Test-Path $optionsDir)) {
+  New-Item -ItemType Directory -Path $optionsDir | Out-Null
+}
+New-Item -ItemType SymbolicLink -Force -Path "$optionsDir\ui.lnf.xml" -Target "$PSScriptRoot\config\ui.lnf.xml" | Out-Null
+
+
 Write-MySuccess -Icon "PHPStorm 配置完成"
 Write-MyInfo ""
