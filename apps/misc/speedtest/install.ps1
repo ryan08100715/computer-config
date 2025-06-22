c84a480c93b3dd87cb0bf48b81fc9dd53a3b9ea0
Ryan WSY <ryan08100715@gmail.com>
2025-06-22 21:21:21 +0800

    Add Speedtest install script

---
+++ apps/misc/speedtest/install.ps1
@@ -0,0 +1,20 @@
+#!/usr/bin/env pwsh
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\winget.ps1
+
+$PackageName = "Speedtest"
+
+Write-MyInfo "開始執行 $PackageName 安裝腳本..."
+
+$osInfo = Get-MyOSInfo
+
+if ($osInfo.OS -eq 'windows') {
+  $WingetPackageID = "Ookla.Speedtest.Desktop"
+
+  Install-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+}
+else {
+  Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
+}
