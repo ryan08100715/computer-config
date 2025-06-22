e797bd818b8a5c9b4c5970da3e386fbc8f75d8ef
Ryan WSY <ryan08100715@gmail.com>
2025-06-22 21:15:54 +0800

    Add postman install script

---
+++ apps/misc/postman/uninstall.ps1
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\winget.ps1
+
+$PackageName = "Postman"
+
+Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
+
+$osInfo = Get-MyOSInfo
+
+if ($osInfo.OS -eq 'windows') {
+  $WingetPackageID = "Postman.Postman"
+
+  Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+}
+else {
+  Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
+}
+
+Write-MyInfo ""
