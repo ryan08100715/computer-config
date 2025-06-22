0dd58bec1816dde8290d8441e6a7bdb35f3548a5
Ryan WSY <ryan08100715@gmail.com>
2025-06-22 21:12:34 +0800

    Add notion install script

---
+++ apps/note_taking_software/notion/uninstall.ps1
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+
+. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\winget.ps1
+
+$PackageName = "Notion"
+
+Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
+
+$osInfo = Get-MyOSInfo
+
+if ($osInfo.OS -eq 'windows') {
+  $WingetPackageID = "Notion.Notion"
+
+  Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
+}
+else {
+  Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
+}
+
+Write-MyInfo ""
