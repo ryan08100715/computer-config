0ce69dc2fcc86f5bff9863daed7e449ba415e05c
Ryan WSY <ryan08100715@gmail.com>
2025-06-28 16:53:28 +0800

    Refactoring golang and rust scripts

--- apps/programs/rustup/uninstall.ps1
+++ apps/programs/rustup/uninstall.ps1
@@ -1,13 +1,14 @@
 #!/usr/bin/env pwsh
 
 . $PSScriptRoot\..\..\..\utils\output.ps1
-. $PSScriptRoot\..\..\..\utils\winget.ps1
+. $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
 
-$PackageName = "Rustup"
-$WingetPackageID = "Rustlang.Rustup"
+$osInfo = Get-MyOSInfo
 
-Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
-
-Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
-
-Write-MyInfo ""
+if ($osInfo.OS -eq 'windows') {
+  UnInstall-MyWinGetPackage -Id "Rustlang.Rustup"
+}
+else {
+  Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
+}
