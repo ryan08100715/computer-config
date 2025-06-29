36ad1978c745426563fa0bc7d8de86c9559c2985
Ryan WSY <ryan08100715@gmail.com>
2025-06-29 18:49:38 +0800

    Fix wsl config

--- apps/wsl/config_setup.ps1
+++ apps/wsl/config_setup.ps1
@@ -1,8 +1,9 @@
 #!/usr/bin/env pwsh
 
-. $PSScriptRoot\..\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\utils\output.ps1
+. $PSScriptRoot\..\..\utils\file_system.ps1
 
-Copy-Item -Force -Path "$PSScriptRoot\config\.wslconfig" -Destination "$HOME\.wslconfig"
+Copy-MyItem -Target "$PSScriptRoot\config\.wslconfig" -Destination "$HOME\.wslconfig" -SuppressOutput
 Write-MySuccess -Icon "放置 .wslconfig 到: $HOME\.wslconfig"
 
 Write-MyInfo ""
