8fb4d7a64e6a3bd6137c66a04302e31799a1ab4e
Ryan WSY <ryan08100715@gmail.com>
2025-07-02 09:11:40 +0800

    Install required before window_virtual_desktop_helper

--- personalization/workspaces/setup.ps1
+++ personalization/workspaces/setup.ps1
@@ -23,6 +23,7 @@ Write-MyInfo "開始執行初始化工作區腳本..."
 Init-WorkspacesCount -Count 10
 
 # 添加工作列顯示目前工作區
+& "$PSScriptRoot\..\..\apps\device_driver\dotnet_framework\install.ps1"
 & "$PSScriptRoot\..\..\apps\basic\windows_virtual_desktop_helper\install.ps1"
 & "$PSScriptRoot\..\..\apps\basic\windows_virtual_desktop_helper\config_setup.ps1"
 
