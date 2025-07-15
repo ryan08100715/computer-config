178379edd75c74fca6e8c08f45002ee8358be879
Ryan WSY <ryan08100715@gmail.com>
2025-07-15 23:10:03 +0800

    docker desktop support choco

--- apps/virtualization/docker_desktop/install.ps1
+++ apps/virtualization/docker_desktop/install.ps1
@@ -1,13 +1,30 @@
 #!/usr/bin/env pwsh
 
+param (
+  [Parameter()]
+  [ValidateSet("winget", "choco")]
+  [string]$WinPackageManager = "winget"
+)
+
 . $PSScriptRoot\..\..\..\utils\output.ps1
 . $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
 . $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\choco.ps1
 
 $osInfo = Get-MyOSInfo
 
 if ($osInfo.OS -eq 'windows') {
-  Install-MyWinGetPackage -Id "Docker.DockerDesktop"
+  if ($WinPackageManager -eq "winget") {
+    Install-MyWinGetPackage -Id "Docker.DockerDesktop"
+  }
+  elseif ($WinPackageManager -eq "choco") {
+    Exit-WhenCommandNotExist -Command choco
+    Install-MyChocoPackage -Id "docker-desktop"
+  }
+  else {
+    Write-MyWarning -Icon "不支援的安裝方式: $WinPackageManager"
+  }
 }
 else {
   Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
--- apps/virtualization/docker_desktop/uninstall.ps1
+++ apps/virtualization/docker_desktop/uninstall.ps1
@@ -1,13 +1,30 @@
 #!/usr/bin/env pwsh
 
+param (
+  [Parameter()]
+  [ValidateSet("winget", "choco")]
+  [string]$WinPackageManager = "winget"
+)
+
 . $PSScriptRoot\..\..\..\utils\output.ps1
 . $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
 . $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\choco.ps1
 
 $osInfo = Get-MyOSInfo
 
 if ($osInfo.OS -eq 'windows') {
-  UnInstall-MyWinGetPackage -Id "Docker.DockerDesktop"
+  if ($WinPackageManager -eq "winget") {
+    UnInstall-MyWinGetPackage -Id "Docker.DockerDesktop"
+  }
+  elseif ($WinPackageManager -eq "choco") {
+    Exit-WhenCommandNotExist -Command choco
+    UnInstall-MyChocoPackage -Id "docker-desktop"
+  }
+  else {
+    Write-MyWarning -Icon "不支援的解除安裝方式: $WinPackageManager"
+  }
 }
 else {
   Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
