5b0791c7e6b205810c21d467236a0f1e235de1d6
Ryan WSY <ryan08100715@gmail.com>
2025-07-15 22:56:45 +0800

    windows terminal support choco

--- apps/terminal/windows_terminal/install.ps1
+++ apps/terminal/windows_terminal/install.ps1
@@ -1,13 +1,30 @@
 #!/usr/bin/env pwsh
 
+param (
+  [Parameter()]
+  [ValidateSet("winget", "choco")]
+  [string]$WinPackageManager = "winget"
+)
+
 . $PSScriptRoot\..\..\..\utils\output.ps1
 . $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
 . $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\choco.ps1
 
 $osInfo = Get-MyOSInfo
 
 if ($osInfo.OS -eq 'windows') {
-  Install-MyWinGetPackage -Id "Microsoft.WindowsTerminal"
+  if ($WinPackageManager -eq "winget") {
+    Install-MyWinGetPackage -Id "Microsoft.WindowsTerminal"
+  }
+  elseif ($WinPackageManager -eq "choco") {
+    Exit-WhenCommandNotExist -Command choco
+    Install-MyChocoPackage -Id "microsoft-windows-terminal"
+  }
+  else {
+    Write-MyWarning -Icon "不支援的安裝方式: $WinPackageManager"
+  }
 }
 else {
   Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
