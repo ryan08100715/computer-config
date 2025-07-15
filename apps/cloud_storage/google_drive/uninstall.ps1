8bca1abcbe6fee54b838685d352b9e0249bd04a7
Ryan WSY <ryan08100715@gmail.com>
2025-07-15 23:23:42 +0800

    googledrive、megasync support choco

--- apps/cloud_storage/google_drive/uninstall.ps1
+++ apps/cloud_storage/google_drive/uninstall.ps1
@@ -1,15 +1,31 @@
 #!/usr/bin/env pwsh
 
+param (
+  [Parameter()]
+  [ValidateSet("winget", "choco")]
+  [string]$WinPackageManager = "winget"
+)
+
 . $PSScriptRoot\..\..\..\utils\output.ps1
 . $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
 . $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\choco.ps1
 
 $osInfo = Get-MyOSInfo
 
 if ($osInfo.OS -eq 'windows') {
-  UnInstall-MyWinGetPackage -Id "Google.GoogleDrive"
+  if ($WinPackageManager -eq "winget") {
+    UnInstall-MyWinGetPackage -Id "Google.GoogleDrive"
+  }
+  elseif ($WinPackageManager -eq "choco") {
+    Exit-WhenCommandNotExist -Command choco
+    UnInstall-MyChocoPackage -Id "googledrive"
+  }
+  else {
+    Write-MyWarning -Icon "不支援的解除安裝方式: $WinPackageManager"
+  }
 }
 else {
   Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
 }
-
