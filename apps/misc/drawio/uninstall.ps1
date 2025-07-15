3cb0f0c1e484d7c7cba818e5ecfe7ee5ae55fad4
Ryan WSY <ryan08100715@gmail.com>
2025-07-15 23:12:52 +0800

    drawio、zerotier support choco

--- apps/misc/drawio/uninstall.ps1
+++ apps/misc/drawio/uninstall.ps1
@@ -1,13 +1,31 @@
 #!/usr/bin/env pwsh
 
+param (
+  [Parameter()]
+  [ValidateSet("winget", "choco")]
+  [string]$WinPackageManager = "winget"
+)
+
 . $PSScriptRoot\..\..\..\utils\output.ps1
-. $PSScriptRoot\..\..\..\utils\winget.ps1
+. $PSScriptRoot\..\..\..\utils\os.ps1
+. $PSScriptRoot\..\..\..\utils\utils.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\winget.ps1
+. $PSScriptRoot\..\..\..\utils\package_manager\choco.ps1
 
-$PackageName = "draw.io"
-$WingetPackageID = "JGraph.Draw"
+$osInfo = Get-MyOSInfo
 
-Write-MyInfo "開始執行 $PackageName 解除安裝腳本..."
-
-Uninstall-MyWinGetPackage -PackageName $PackageName -WingetPackageID $WingetPackageID
-
-Write-MyInfo ""
+if ($osInfo.OS -eq 'windows') {
+  if ($WinPackageManager -eq "winget") {
+    UnInstall-MyWinGetPackage -Id "JGraph.Draw"
+  }
+  elseif ($WinPackageManager -eq "choco") {
+    Exit-WhenCommandNotExist -Command choco
+    UnInstall-MyChocoPackage -Id "drawio"
+  }
+  else {
+    Write-MyWarning -Icon "不支援的解除安裝方式: $WinPackageManager"
+  }
+}
+else {
+  Write-MyWarning -Icon "不支援的作業系統: $($osInfo.OS)"
+}
