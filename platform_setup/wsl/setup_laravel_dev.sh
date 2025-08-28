9712494b2f36307f76261b77157028eda5a7c9c6
Ryan WSY <ryan08100715@gmail.com>
2025-08-28 08:59:56 +0800

    更新 laravel 環境

--- platform_setup/wsl/setup_laravel_dev.sh
+++ platform_setup/wsl/setup_laravel_dev.sh
@@ -19,8 +19,9 @@ sudo apt-get update
 sudo apt-get install -y php${PHP_VERSION} php${PHP_VERSION}-fpm
 
 # * 安裝 PHP Extensions
-sudo apt-get install -y php${PHP_VERSION}-curl php${PHP_VERSION}-mbstring php${PHP_VERSION}-xml php${PHP_VERSION}-intl
+sudo apt-get install -y php${PHP_VERSION}-curl php${PHP_VERSION}-mbstring php${PHP_VERSION}-xml php${PHP_VERSION}-intl php${PHP_VERSION}-gd php${PHP_VERSION}-bcmath
 sudo apt-get install -y php${PHP_VERSION}-sqlite3 php${PHP_VERSION}-mysql php${PHP_VERSION}-pgsql php${PHP_VERSION}-redis
+sudo apt-get install -y php${PHP_VERSION}-zip php${PHP_VERSION}-xml php${PHP_VERSION}-gd
 sudo apt-get install -y php${PHP_VERSION}-xdebug php${PHP_VERSION}-zip
 
 # * 安裝 Composer
