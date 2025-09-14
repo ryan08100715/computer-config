5bae18e4cf0437c96ada9f427232218f4516f954
Ryan WSY <ryan08100715@gmail.com>
2025-09-14 16:43:17 +0800

    Change window setup

--- platform_setup/windows/setup_home_pc.ps1
+++ platform_setup/windows/setup_home_pc.ps1
@@ -44,6 +44,9 @@ $apps = @(
   #'apps\note_taking_software\obsidian\install.ps1'
   #'apps\note_taking_software\notion\install.ps1'
 
+  # * 密碼管理工具
+  'apps\misc\1password\install.ps1'
+
   # * 瀏覽器
   'apps\browser\google_chrome\install.ps1'
 
