3c6caba0bf5435d866377a45cdd098542b647c15
Ryan WSY <ryan08100715@gmail.com>
2025-09-17 21:25:26 +0800

    Update powershell config

--- apps/shell/powershell7/config/Microsoft.PowerShell_profile.ps1
+++ apps/shell/powershell7/config/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,33 @@ Clear-Host
 # Tab 補全顯示全部選項
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
+# 自定義 Tab 補全
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock {
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    
+    # 定義縮寫映射
+    $abbreviations = @{
+        'pa' = 'php artisan'
+        'hpa' = 'herd php artisan'
+    }
+    
+    # 檢查當前輸入是否匹配縮寫
+    $words = $line.Split(' ')
+    $lastWord = $words[-1]
+    
+    if ($abbreviations.ContainsKey($lastWord)) {
+        $expansion = $abbreviations[$lastWord]
+        $newLine = $line.Substring(0, $cursor - $lastWord.Length) + $expansion
+        [Microsoft.PowerShell.PSConsoleReadLine]::Replace(0, $line.Length, $newLine)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($newLine.Length)
+    } else {
+        # 如果不匹配縮寫，執行正常的 Tab 補全
+        [Microsoft.PowerShell.PSConsoleReadLine]::TabCompleteNext()
+    }
+}
+
 # 預測顯示為多行列表，預設為 Inline
 Set-PSReadLineOption -PredictionViewStyle ListView
 
