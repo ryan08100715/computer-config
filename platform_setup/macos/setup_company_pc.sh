0f748af4b3995406fb9bb949f3c78f643d8a587f
Ryan WSY <ryan08100715@gmail.com>
2026-05-09 00:09:22 +0800

    調整 mac 配置腳本

--- platform_setup/macos/setup_company_pc.sh
+++ platform_setup/macos/setup_company_pc.sh
@@ -3,5 +3,13 @@
 SCRIPT_DIR=$(dirname "$0")
 APPS_DIR="$SCRIPT_DIR/../../apps"
 
+# * 安裝 fish
+bash "$APPS_DIR/shell/fish/macos/install.sh"
+bash "$APPS_DIR/shell/fish/macos/config_setup.sh"
+
+# * 安裝 starship
+bash "$APPS_DIR/shell/starship/macos/install.sh"
+bash "$APPS_DIR/shell/starship/macos/config_setup.sh"
+
 # * 安裝 iTerm2
 bash "$APPS_DIR/terminal/iterm2_terminal/macos/install.sh"
