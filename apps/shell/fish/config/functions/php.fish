4ef608e41ee5c57e03a99d52d3023ca66574a1b9
Ryan WSY <ryan08100715@gmail.com>
2026-05-11 12:55:12 +0800

    調整 fish function php

--- apps/shell/fish/config/functions/php.fish
+++ apps/shell/fish/config/functions/php.fish
@@ -1,7 +1,8 @@
 # herd 已安裝才執行
-if type -q herd
-  function php --wraps='herd php' --description 'alias php=herd php'
-    herd php $argv      
+function php --wraps='herd php' --description 'alias php=herd php'
+  if type -q herd
+    herd php $argv
+  else
+    command php $argv
   end
 end
-
